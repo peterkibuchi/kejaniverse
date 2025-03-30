91d3a307988627c34ccc519fd95109c9997a9ead
Glen Ochieng <glenochieng045@gmail.com>
2025-03-30 22:03:56 +0300

    feat: modified `createProperty` action to match updated schema

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -4,28 +4,46 @@ import "server-only";
 
 import { auth } from "@clerk/nextjs/server";
 
-import { type CreatePropertyFormData } from "~/app/(dashboard)/properties/new/context";
+import { type CreatePropertyFormContextType } from "~/app/(dashboard)/properties/new/context";
 import { db } from "~/server/db";
-import { property } from "~/server/db/schema";
+import { property, unitType } from "~/server/db/schema";
 
 export async function createProperty({
   propertyName,
   bankAccountNumber,
-}: CreatePropertyFormData) {
+  unitTypes,
+}: CreatePropertyFormContextType) {
   const { userId } = await auth();
   if (!userId) {
     throw new Error("User not authenticated");
   }
 
-  const result = await db
-    .insert(property)
-    .values({
-      ownerId: userId,
-      name: propertyName,
-      bankAccountNo: bankAccountNumber,
-    })
-    .returning({ insertedId: property.id });
+  const result = await db.transaction(async (tx) => {
+    const results = await tx
+      .insert(property)
+      .values({
+        name: propertyName,
+        bankAccountNumber,
+        ownerId: userId,
+      })
+      .returning({ id: property.id });
 
-  console.log(result);
-  return result[0]?.insertedId;
+    const propertyId = results[0]?.id;
+    if (!propertyId) {
+      tx.rollback();
+      throw new Error("Failed to create property");
+    }
+
+    await tx.insert(unitType).values(
+      unitTypes.map((ut) => ({
+        unitType: ut.unitType,
+        rentPrice: ut.rentPrice,
+        propertyId,
+      })),
+    );
+
+    return propertyId;
+  });
+
+  return result;
 }
